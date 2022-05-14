30729d4609ce9ec1b529e46765c6679707a89fe5
DBprassan12 <divij.bhaw15@gmail.com>
2022-05-14 20:24:11 +0800

    thirdC

--- Task 1/script.sh
+++ Task 1/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-File="./sample/sample1.txt"
+File="./sample/sample2.txt"
 
 
 declare -a StringArray=( "also" "although" "and" "as" "because" "before" "but" "for" "if" "nor" "of" "or" "since" "that" "though"  "until"  "when" "whenever" "whereas" "which"  "while"  "yet" )
@@ -14,5 +14,33 @@ do
     foundInfiles=$(grep -ow "$val" "$File" | wc -l)
     echo $val $foundInfiles
     
-    
 done
+
+
+
+WordCount=$( wc -w $File |  cut -d ' ' -f1   )
+echo Words "$WordCount"
+
+commas=$(grep -o "," "$File" | wc -l)
+echo comma "$commas"
+
+Semicolon=$(grep -o ";" "$File" | wc -l)
+echo Semi_colon "$Semicolon"
+
+sentence=$(grep -ow "." "$File" | wc -l)
+echo sentence "$sentence"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
