e470308f27e8ded4f57b2937f38802bd65d09de5
DBprassan12 <divij.bhaw15@gmail.com>
2022-05-14 16:43:41 +0800

    secondC

--- Task 1/script.sh
+++ Task 1/script.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-
+File="./sample/sample1.txt"
 
 
 declare -a StringArray=( "also" "although" "and" "as" "because" "before" "but" "for" "if" "nor" "of" "or" "since" "that" "though"  "until"  "when" "whenever" "whereas" "which"  "while"  "yet" )
 
 # Iterate the string array using for loop
+
+
 for val in ${StringArray[@]};
 
 do
-    
-    
-    echo $val
+    foundInfiles=$(grep -ow "$val" "$File" | wc -l)
+    echo $val $foundInfiles
     
     
 done
